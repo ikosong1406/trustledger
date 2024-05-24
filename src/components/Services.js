6127a9fc8f09641e569af7e1089bb6acca0f21da
Alexandra <alexandervirtuous14@gmail.com>
2024-05-24 21:31:52 +0100

    deleted:    src/components/CandlestickChart.js 	deleted:    src/components/Constant.js 	deleted:    src/components/Piechart.js 	deleted:    src/components/Services.js 	deleted:    src/components/Utils.js 	modified:   src/pages/Main.jsx 	modified:   src/styles/Main.css

--- src/components/Services.js
+++
@@ -1,9 +0,0 @@
-const VITE_API_KEY = "G1ZAQLRST5V1ZATU";
-
-export const fetchStockData = async (symbol) => {
-  const response = await fetch(
-    `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=${symbol}&apikey=${VITE_API_KEY}`
-  );
-  const data = await response.json();
-  return data;
-};
