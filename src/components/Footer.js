a4f73e6953e9374174ce9ef92dbf6ecbb05a74d4
Alexandra <alexandervirtuous14@gmail.com>
2024-06-08 12:01:24 +0100

    modified:   src/components/Footer.js 	modified:   src/styles/Riskwarning.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ const Footer = () => {
           <div className="footerDiv4">
             <div className="footerDiv3">
               <h2 className="legal">Legal</h2>
-              <Link to="/privacypolicy" className="legal1">
+              <Link to="/privacy" className="legal1">
                 Privacy Policy
               </Link>
               <Link to="/termsofuse" className="legal1">
@@ -30,7 +30,7 @@ const Footer = () => {
               <Link to="/security" className="legal1">
                 Security
               </Link>
-              <Link to="/riskwarning" className="legal1">
+              <Link to="/risk" className="legal1">
                 Risk Warning
               </Link>
             </div>
