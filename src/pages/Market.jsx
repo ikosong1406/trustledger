e87e1ff1074fd0909ab062373a694569592e96a3
Alexandra <alexandervirtuous14@gmail.com>
2024-06-11 21:29:42 +0100

    modified:   src/pages/Market.jsx

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -100,16 +100,6 @@ const Market = () => {
           type="candlestick"
           height={350}
         />
-        {/* <LineChart
-          xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-          series={[
-            {
-              data: [2, 5.5, 2, 8.5, 1.5, 5],
-            },
-          ]}
-          width={500}
-          height={300}
-        /> */}
       </div>
     </div>
   );
