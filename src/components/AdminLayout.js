93d8961c0cf06e480b01ebaa4be5f2d9c2362b53
Alexandra <alexandervirtuous14@gmail.com>
2024-06-21 00:44:17 +0100

    modified:   src/App.js 	modified:   src/components/AdminLayout.js

--- src/components/AdminLayout.js
+++ src/components/AdminLayout.js
@@ -1,28 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import AdminSide from "./AdminSide";
-import AdminTab from "./AdminTab";
 import "../styles/Layout.css";
-import { useWindowDimensions } from "./useWindowDimensions";
 
 const AdminLayout = () => {
-  const { width } = useWindowDimensions();
-
   return (
     <div className="layout-container">
-      {width > 925 ? (
-        <div className="sidebar-container">
-          <AdminSide />
-        </div>
-      ) : null}
+      <div className="sidebar-container">
+        <AdminSide />
+      </div>
       <div className="page-container">
         <Outlet />
       </div>
-      {width <= 925 ? (
-        <div className="bottom-tab-container">
-          <AdminTab />
-        </div>
-      ) : null}
     </div>
   );
 };
