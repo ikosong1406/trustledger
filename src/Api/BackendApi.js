57421416a0669265d48d7dfe88dd2b0cfe7678a4
Alexandra <alexandervirtuous14@gmail.com>
2024-06-24 11:40:44 +0100

    modified:   src/Api/BackendApi.js

--- src/Api/BackendApi.js
+++ src/Api/BackendApi.js
@@ -1,5 +1,6 @@
 const BackendApi =
   // "http://192.168.153.16:5001";
-  "https://trustledgerbackend.onrender.com";
+  // "https://trustledgerbackend.onrender.com";
+  "https://handy-office-flashy-water-production.pipeops.app";
 
 export default BackendApi;
