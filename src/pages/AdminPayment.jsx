11aaca36fd04d5f2d094c89e7e226630607f5b24
Alexandra <alexandervirtuous14@gmail.com>
2024-06-25 16:15:59 +0100

    modified:   src/pages/AdminPayment.jsx

--- src/pages/AdminPayment.jsx
+++ src/pages/AdminPayment.jsx
@@ -1,55 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/AdminPayment.css";
 import Colors from "../components/Colors";
 import { FcSimCardChip } from "react-icons/fc";
+import axios from "axios";
+import api from "../Api/BackendApi";
 
 const AdminPayment = () => {
-  const [paymentMethods, setPaymentMethods] = useState([
-    { id: 1, type: "USDT", address: "USDT-Wallet-Address", limit: 1000 },
-    { id: 2, type: "Bitcoin", address: "Bitcoin-Wallet-Address", limit: 2000 },
-  ]);
+  const [method, setMethod] = useState([]);
 
-  const [withdrawalMethods, setWithdrawalMethods] = useState([
-    { id: 1, type: "USDT", address: "USDT-Wallet-Address", limit: 500 },
-    { id: 2, type: "Bitcoin", address: "Bitcoin-Wallet-Address", limit: 1000 },
-    { id: 3, type: "PayPal", address: "PayPal-Email", limit: 1500 },
-  ]);
+  useEffect(() => {
+    const fetchMethod = async () => {
+      try {
+        const response = await axios.get(`${api}/allMethod`);
+        setMethod(response.data);
+      } catch (error) {
+        console.error("Error fetching transactions:", error);
+      }
+    };
+
+    fetchMethod();
+  }, []);
 
   const [newPaymentMethod, setNewPaymentMethod] = useState({
     type: "",
     address: "",
     limit: "",
   });
-
-  const [newWithdrawalMethod, setNewWithdrawalMethod] = useState({
-    type: "",
-    address: "",
-    limit: "",
-  });
-
-  const handleChange = (e, setMethod) => {
-    const { name, value } = e.target;
-    setMethod((prevMethod) => ({
-      ...prevMethod,
-      [name]: value,
-    }));
-  };
-
-  const addPaymentMethod = () => {
-    setPaymentMethods([
-      ...paymentMethods,
-      { id: Date.now(), ...newPaymentMethod },
-    ]);
-    setNewPaymentMethod({ type: "", address: "", limit: "" });
-  };
-
-  const addWithdrawalMethod = () => {
-    setWithdrawalMethods([
-      ...withdrawalMethods,
-      { id: Date.now(), ...newWithdrawalMethod },
-    ]);
-    setNewWithdrawalMethod({ type: "", address: "", limit: "" });
-  };
 
   return (
     <div className="adHomeMain" style={{ minHeight: 500 }}>
@@ -62,15 +38,15 @@ const AdminPayment = () => {
         <div className="methods-container">
           <h2 style={{ color: "gray" }}>Deposit Methods</h2>
           <div className="card-container">
-            {paymentMethods.map((method) => (
-              <div className="adHomeDiv21" key={method.id}>
+            {method.map((data) => (
+              <div className="adHomeDiv21" key={data._id}>
                 <div className="adHome211">
-                  <h3>{method.type}</h3>
+                  <h3>{data.name}</h3>
                   <p style={{ color: "gray", fontWeight: 500 }}>
-                    Address: {method.address}
+                    Address: {data.walletAddress}
                   </p>
                   <p style={{ color: "gray", fontWeight: 500 }}>
-                    Limit: ${method.limit}
+                    Network: {data.network}
                   </p>
                 </div>
                 <FcSimCardChip style={{ fontSize: 40, alignSelf: "center" }} />
@@ -78,95 +54,6 @@ const AdminPayment = () => {
             ))}
           </div>
         </div>
-
-        <div className="methods-container">
-          <h2 style={{ color: "gray" }}>Withdrawal Methods</h2>
-          <div className="card-container">
-            {withdrawalMethods.map((method) => (
-              <div className="adHomeDiv21" key={method.id}>
-                <div className="adHome211">
-                  <h3>{method.type}</h3>
-                  <p style={{ color: "gray", fontWeight: 500 }}>
-                    Address: {method.address}
-                  </p>
-                  <p style={{ color: "gray", fontWeight: 500 }}>
-                    Limit: ${method.limit}
-                  </p>
-                </div>
-                <FcSimCardChip style={{ fontSize: 40, alignSelf: "center" }} />
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div className="form-container">
-        <h2>Add Payment Method</h2>
-        <label>
-          Type:
-          <input
-            type="text"
-            name="type"
-            value={newPaymentMethod.type}
-            onChange={(e) => handleChange(e, setNewPaymentMethod)}
-            placeholder="Enter type (e.g., USDT, Bitcoin)"
-          />
-        </label>
-        <label>
-          Address:
-          <input
-            type="text"
-            name="address"
-            value={newPaymentMethod.address}
-            onChange={(e) => handleChange(e, setNewPaymentMethod)}
-            placeholder="Enter wallet address"
-          />
-        </label>
-        <label>
-          Limit:
-          <input
-            type="number"
-            name="limit"
-            value={newPaymentMethod.limit}
-            onChange={(e) => handleChange(e, setNewPaymentMethod)}
-            placeholder="Enter limit amount"
-          />
-        </label>
-        <button onClick={addPaymentMethod}>Add Payment Method</button>
-      </div>
-
-      <div className="form-container">
-        <h2>Add Withdrawal Method</h2>
-        <label>
-          Type:
-          <input
-            type="text"
-            name="type"
-            value={newWithdrawalMethod.type}
-            onChange={(e) => handleChange(e, setNewWithdrawalMethod)}
-            placeholder="Enter type (USDT, Bitcoin, PayPal)"
-          />
-        </label>
-        <label>
-          Address:
-          <input
-            type="text"
-            name="address"
-            value={newWithdrawalMethod.address}
-            onChange={(e) => handleChange(e, setNewWithdrawalMethod)}
-            placeholder="Enter wallet address or email"
-          />
-        </label>
-        <label>
-          Limit:
-          <input
-            type="number"
-            name="limit"
-            value={newWithdrawalMethod.limit}
-            onChange={(e) => handleChange(e, setNewWithdrawalMethod)}
-            placeholder="Enter limit amount"
-          />
-        </label>
-        <button onClick={addWithdrawalMethod}>Add Withdrawal Method</button>
       </div>
     </div>
   );
