c47cd7e58bfb8502741d6451b568833b0836bb59
Alexandra <alexandervirtuous14@gmail.com>
2024-07-03 00:22:54 +0100

    modified:   src/Api/BackendApi.js

--- src/Api/BackendApi.js
+++ src/Api/BackendApi.js
@@ -1,6 +1,3 @@
-const BackendApi =
-  // "http://192.168.153.16:5001";
-  // "https://trustledgerbackend.onrender.com";
-  "https://soft-cabbage-flashy-water-production.pipeops.app";
+const BackendApi = "https://soft-cabbage-flashy-water-production.pipeops.app";
 
 export default BackendApi;
